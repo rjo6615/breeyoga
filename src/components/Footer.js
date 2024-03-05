67a548d591965a44195199c5b26af41a799d84cc
John Miller <john.ifert.miller@gmail.com>
2024-03-04 20:16:06 -0500

    messed with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,117 +1,71 @@
 function Footer() {
     return (       
         <div>
-        <footer className="text-center text-black" style={{  backgroundImage: 'linear-gradient(to bottom, #e1c684, #90adc6)'}}>
+        <footer className="text-center text-black" style={{  backgroundImage: 'linear-gradient(to bottom, #fff6ea, #90adc6)'}}>
           <div className="container p-4">
             <section className="mb-4">
-
-              <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                ><i className="fab fa-facebook-f"></i></a>
-
-              <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                ><i className="fab fa-twitter"></i></a>
-
-              <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                ><i className="fab fa-instagram"></i></a>
-
-              <a className="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                ><i className="fab fa-linkedin-in"></i></a>
-
             </section>
 
             <section className="mb-4">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-                repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
-                eum harum corrupti dicta, aliquam sequi voluptate quas.
+              <p style={{fontSize: '20px'}}>
+                Immerse yourself in the synergy of yoga and soothing sounds for ultimate wellbeing. 
+                The blend of calming melodies and mindful movements creates a harmonious path to inner peace and balance.
               </p>
             </section>
  
             <section className="">
               <div className="row">
                 <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                  <h5 className="text-uppercase">Links</h5>
+                  <h5 className="">Located In</h5>
         
                   <ul className="list-unstyled mb-0">
                     <li>
-                      <a href="#!" className="text-black">Link 1</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 2</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 3</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 4</a>
+                      <a className="text-black" style={{ textDecoration: 'none' }}>Atlanta Georgia</a>
                     </li>
                   </ul>
                 </div>
      
                 <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                  <h5 className="text-uppercase">Links</h5>
-        
-                  <ul className="list-unstyled mb-0">
-                    <li>
-                      <a href="#!" className="text-black">Link 1</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 2</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 3</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 4</a>
-                    </li>
-                  </ul>
                 </div>
 
                 <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                  <h5 className="text-uppercase">Links</h5>
-        
-                  <ul className="list-unstyled mb-0">
-                    <li>
-                      <a href="#!" className="text-black">Link 1</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 2</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 3</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 4</a>
-                    </li>
-                  </ul>
                 </div>
 
                 <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                  <h5 className="text-uppercase">Links</h5>
+                  <h5 className="">Developer Links</h5>
         
                   <ul className="list-unstyled mb-0">
+
+                  <li>
+                    <span className="text-black">John I-M</span>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/john-ifert-miller-36231126a/" className="icon-link">
+                      <i className="fab fa-linkedin-in" style={{ color: 'black' }}></i>
+                    </a>
+                    &nbsp;
+                    <a href="https://github.com/johnboy514" className="icon-link">
+                      <i className="fab fa-github" style={{ color: 'black' }}></i>
+                    </a>
+                  </li>
+
                     <li>
-                      <a href="#!" className="text-black">Link 1</a>
+                      <span className="text-black">Robert O</span>
+                      &nbsp;
+                      <a href="https://www.linkedin.com/in/robert-obernier-700b36269/" className="icon-link">
+                        <i className="fab fa-linkedin-in" style={{ color: 'black' }}></i>
+                      </a>
+                      &nbsp;
+                      <a href="https://github.com/rjo6615" className="icon-link">
+                        <i className="fab fa-github" style={{ color: 'black' }}></i>
+                      </a>
                     </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 2</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 3</a>
-                    </li>
-                    <li>
-                      <a href="#!" className="text-black">Link 4</a>
-                    </li>
+
                   </ul>
                 </div>
               </div>
             </section>
           </div>
         
-          <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-            © 2020 Copyright:
-            <a className="text-black mx-2" href="/#">Yoga Template</a>
-          </div>
         </footer>
         </div>
     )
